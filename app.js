e707e525bf8a3dcf960a0ee4ae07b5a5e8c25476
Jitgitbit <rowowolf@gmail.com>
2020-04-13 11:28:39 +0200

    flipCard fn made

--- app.js
+++ app.js
@@ -68,14 +68,25 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const grid = document.querySelector('.grid')   // div from html picked up
+  var cardsChosen = []
+  var cardsChosenId = []
 
   function createBoard(){
     for(let i = 0; i < cardArray.length; i++){
-      const card = document.createElement('img')
+      var card = document.createElement('img')
       card.setAttribute('src', 'images/blank.png')
       card.setAttribute('data-id', i)
-      //card.addEventListener('click', flipcard)
+      card.addEventListener('click', flipCard)
       grid.appendChild(card)
+    }
+  }
+  function flipCard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].img)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2){
+      setTimeout(checkForMatch, 500)
     }
   }
   createBoard()
