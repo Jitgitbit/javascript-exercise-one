2f3e09056b0a5ec341099d525d39be2891e38fad
Jitgitbit <rowowolf@gmail.com>
2020-04-13 11:12:01 +0200

    basic createBoard fn done

--- app.js
+++ app.js
@@ -67,6 +67,16 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   ]
 
-  const grid = document.querySelector.apply('.grid')   // div from html picked up
+  const grid = document.querySelector('.grid')   // div from html picked up
 
+  function createBoard(){
+    for(let i = 0; i < cardArray.length; i++){
+      const card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+      //card.addEventListener('click', flipcard)
+      grid.appendChild(card)
+    }
+  }
+  createBoard()
 })
